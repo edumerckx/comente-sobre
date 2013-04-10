0148cea267ec5325e8e772c909ed7c44965b6e8c
edumerckx <edu_merckx@yahoo.com.br>
2013-04-10 01:22:50 -0300

    Correcao classe Tratador

--- src/com/eduardojanuario/comente/utils/Tratador.java
+++ src/com/eduardojanuario/comente/utils/Tratador.java
@@ -11,16 +11,11 @@ public class Tratador {
 	public static String geraUrlParamAmigavel(String param) {
 		
 		String retorno = param;
-		retorno = retorno.replaceAll("[ÈÉÊË]","E");
-	    retorno = retorno.replaceAll("[ÛÙÚÜ]","U");
-	    retorno = retorno.replaceAll("[ÏÎÍÌ]","I");
-	    retorno = retorno.replaceAll("[ÁÀÂÃÄ]","A");
-	    retorno = retorno.replaceAll("[ÔÓÒÕÖ]","O");		
-		retorno = retorno.replaceAll("[èéêë]","e");
-		retorno = retorno.replaceAll("[ûùúü]","u");
-		retorno = retorno.replaceAll("[ïîíì]","i");
-		retorno = retorno.replaceAll("[ãäáàâ]","a");
-		retorno = retorno.replaceAll("[ôõóòö]","o");
+		retorno = retorno.replaceAll("[èéêëÈÉÊË]","e");
+		retorno = retorno.replaceAll("[ûùúüÛÙÚÜ]","u");
+		retorno = retorno.replaceAll("[ïîíìÏÎÍÌ]","i");
+		retorno = retorno.replaceAll("[ãäáàâÁÀÂÃÄ]","a");
+		retorno = retorno.replaceAll("[ôõóòöÔÓÒÕÖ]","o");
 		retorno = retorno.replaceAll(" ", "-");
     	
 		return retorno;
